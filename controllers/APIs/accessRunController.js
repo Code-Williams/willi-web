e8ad45cd71fae61e288946412bc9e67e5c14c7fc
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-03-20 02:19:21 +0330

    Access run controller optimized

--- controllers/APIs/accessRunController.js
+++ controllers/APIs/accessRunController.js
@@ -1,9 +1,18 @@
 const Bot = require("../../models/Bots")
 const templates = require("../../utils/jsonCallBackTemplate")
+const botChecker = require("../../helpers/botFirstCheck")
 
 const post = async(req, res) => {
+    // Client data
+    console.log("---- New Request ----")
+    console.log("Client IP: " + req.ip)
+    console.log("Have client: " + !!req.body.client)
+    console.log("Client Id: " + (req.body.client ? req.body.client.id : "-"))
+    console.log("---- End Request ----")
+
     const client = req.body.client
 
+    // Request comes but haven't client
     if(!client){
         return res.json({
             ok : false,
@@ -12,32 +21,16 @@ const post = async(req, res) => {
     }
 
     // If bot is not registered in database
-    const isBotRegistered = await Bot.findOne({ where : { botId : client.id } })
-    if(!isBotRegistered) {
-        const callBackData = templates.access_run
-        callBackData.ok = false
-        callBackData.isBotRegistered = false
-        callBackData.message = "Your bot is not registered"
-        return res.json(callBackData)
+    const isBotRegistered = await Bot.findOne({ where : { botId : client.id } }) // Finding bot in database
+    const botValidationResult = botChecker(isBotRegistered) // Check bot validation rules
+
+    if(!botValidationResult){ // If bot have not any data in database
+        const invalidTemplate = templates.access_run
+        invalidTemplate.message = "Your bot is not registered"
+        return res.json(invalidTemplate)
     }
 
-    // If bot is blocked
-    if(isBotRegistered.blocked) {
-        const callBackData = templates.access_run
-        callBackData.message = "Your bot is blocked. Please check your dashboard"
-        callBackData.blocked = true
-        callBackData.isBotRegistered = true
-        return res.json(callBackData)
-    }
-
-    // If bot have bills upper than 100K Tomans
-    if(isBotRegistered.bills >= 100000) {
-        const callBackData = templates.access_run
-        callBackData.message = "Your bot have bills. Please check your dashboard"
-        callBackData.bills = true
-        callBackData.isBotRegistered = true
-        return res.json(callBackData)
-    }
+    if(!botValidationResult.ok) return res.json(botValidationResult) // If bot is blocked or have bills (bot is not ok)
 
     // Bot have access to run
     const callBackData = templates.access_run
