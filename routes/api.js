64a76adc772276e67553742de9792fe4072ff193
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-06-11 17:20:46 +0430

    API name changed

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,6 @@ const accessRunController = require("../controllers/APIs/accessRunController")
 Router.post("/v1/access_run", accessRunController.post)
 
 const accessRunProjectController = require("../controllers/APIs/accessRunProjectController")
-Router.post("/v1/access_run_proj", accessRunProjectController.post)
+Router.post("/v1/access_run_server", accessRunProjectController.post)
 
 module.exports = Router
